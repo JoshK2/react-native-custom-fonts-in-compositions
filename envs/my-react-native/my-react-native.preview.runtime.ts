eeeebc218dfd07a457c5aef214b7281ee9e6e010
Josh Kuttler <yoshuakuttler@gmail.com>
2021-12-22 13:36:55 +0200

    update react-native preview to use preview from react-native aspect

--- envs/my-react-native/my-react-native.preview.runtime.ts
+++ envs/my-react-native/my-react-native.preview.runtime.ts
@@ -1,17 +1,17 @@
 import { PreviewRuntime } from '@teambit/preview';
-import { ReactAspect, ReactPreview } from '@teambit/react';
+import { ReactNativeAspect, ReactNativePreview } from '@teambit/react-native';
 import { ThemeCompositions } from '@company/scope.theme.theme-compositions';
 import { MyReactNativeAspect } from './my-react-native.aspect';
 
 export class MyReactNativePreviewMain {
   static runtime = PreviewRuntime;
 
-  static dependencies = [ReactAspect];
+  static dependencies = [ReactNativeAspect];
 
-  static async provider([react]: [ReactPreview]) {
+  static async provider([reactNative]: [ReactNativePreview]) {
     const myReactNativePreviewMain = new MyReactNativePreviewMain();
     // register a new provider to wrap all compositions using this environment with a custom theme.
-    react.registerProvider([ThemeCompositions]);
+    reactNative.registerProvider([ThemeCompositions]);
 
     return myReactNativePreviewMain;
   }
